d907d41452adfcd87069931250731c43d5a07b1e
xxxxxx <xxxxxx@gmail.com>
2023-04-25 01:24:54 +0900

    change of fontfamily

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout from "../components/layout";
 import Typewriter from "typewriter-effect";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
