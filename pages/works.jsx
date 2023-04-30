07184878e1a77dab20a7a6edf239df10c467fcdd
xxxxxx <xxxxxx@gmail.com>
2023-05-01 02:49:50 +0900

    initial commit

--- pages/works.jsx
+++ pages/works.jsx
@@ -11,12 +11,12 @@ export default function works({ allWorksData }) {
         {allWorksData.map(({ id, date, title, image }) => (
           <li key={id} style={{ borderBottom: "1px solid #1a1a1a" }}>
             <Link href={`/works/${id}`} style={{ display: "flex", width: "100%", padding: "20px 0" }}>
-              <div style={{ display: "flex", justifyContent: "center", alignContent: "center", width: "100px", height: "100px", borderRadius: "6px", overflow: "hidden" }}>
-                <Image src={image} alt="alt" width={600} height={400} priority />
+              <div style={{ display: "flex", justifyContent: "center", alignContent: "center", width: "100px", height: "100px", borderRadius: "6px", backgroundColor: "#1a1a1a", overflow: "hidden" }}>
+                {/* <Image src={image} alt="alt" width={600} height={400} priority /> */}
               </div>
-              <div style={{ width: "calc(100% - 100px)", marginLeft: "20px" }}>
+              <div style={{ width: "calc(100% - 120px)", marginLeft: "20px", padding: "10px 0" }}>
                 <p style={{ fontSize: "14px", fontWeight: "600", color: "#d0d0d0" }}>{title}</p>
-                <span style={{ fontSize: "12px", color: "#a0a0a0" }}>詳しく見る</span>
+                <span style={{ display: "block", marginTop: "20px", fontSize: "12px", color: "#a0a0a0", textAlign: "right" }}>詳しく見る</span>
               </div>
             </Link>
           </li>
