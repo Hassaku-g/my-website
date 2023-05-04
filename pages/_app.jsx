55d78c860ab2c42e9ebb5b70107c2da9cbbcae1d
xxxxxx <xxxxxx@gmail.com>
2023-05-04 21:08:08 +0900

    a

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,8 +3,9 @@ import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps, router }) {
   return (
-    <AnimatePresence mode="wait" initial={true}>
-      <Component {...pageProps} key={router.asPath} />
-    </AnimatePresence>
+    // <AnimatePresence mode="wait" initial={true}>
+    //   <Component {...pageProps} key={router.asPath} />
+    // </AnimatePresence>
+    <Component {...pageProps} />
   );
 }
