eb80df731d9b177a249ec07eeb5cef54edb32b39
xxxxxx <xxxxxx@gmail.com>
2023-05-15 08:03:34 +0900

    commit

--- pages/works.jsx
+++ pages/works.jsx
@@ -19,16 +19,39 @@ export default function Works({ allWorksData }) {
   return (
     <Layout>
       <h1>Works</h1>
-      {allWorksData.map(({ id, title, date, src }) => (
-        <article key={id}>
-          <Link href={`/works/${id}`}>
-            <div className="flex align-center justify-center w-full h-56 border border-neutral-800 rounded bg-neutral-900 overflow-hidden">
-              {src ? <Image className="object-cover" src={src} alt="alt" width={500} height={400} priority /> : null}
-            </div>
-            <p className="mt-2 text-sm text-neutral-400">{title}</p>
-          </Link>
-        </article>
-      ))}
+      <div className="sm:grid-cols-2 gap-2.5 grid">
+        {allWorksData.map(({ id, title, date, src }) => (
+          <article key={id}>
+            <Link href={`/works/${id}`} className="flex max-w-xs flex-col	mx-auto">
+              <div className="flex align-center justify-center w-full h-56 border border-neutral-800 rounded bg-neutral-900 overflow-hidden">
+                {src ? <Image className="object-cover" src={src} alt="alt" width={500} height={400} priority /> : null}
+              </div>
+              <div>
+                <div>
+                  <h3>{title}</h3>
+                  <div
+                    className="card"
+                    style={{
+                      fontSize: "14px",
+                      lineHeight: "20px",
+                      letterSpacing: "-.01em",
+                      display: "-webkit-box",
+                      overflow: "hidden",
+                      marginTop: "8px",
+                    }}
+                  >
+                    Simple, powerful and flexible markdown-powered docs site. Built with Next.js.
+                  </div>
+                </div>
+                <div>
+                  <p className="mt-2 text-sm text-neutral-400">{title}</p>
+                  <button></button>
+                </div>
+              </div>
+            </Link>
+          </article>
+        ))}
+      </div>
     </Layout>
   );
 }
