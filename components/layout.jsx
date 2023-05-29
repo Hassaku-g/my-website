23270ad9b131cf19cead74026ad2e848689ca78b
dipple.1028@icloud.com <123621742+Hassaku-g@users.noreply.github.com>
2023-05-29 23:46:14 +0900

    add page transition animation

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,19 @@
 import Head from "next/head";
 import Navbar from "./navbar";
+import { motion } from "framer-motion";
 
 export default function layout({ children }) {
   return (
     <div className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
       <Navbar />
-      <main className="mt-6 md:mt-0 px-2 md:px-0 mt-12">{children}</main>
+      <motion.main
+        initial={{ opacity: 0, y: 10 }} // 初期状態
+        animate={{ opacity: 1, y: 0 }} // マウント時
+        exit={{ opacity: 0, y: 10 }}
+        className="mt-6 md:mt-0 px-2 md:px-0 mt-12"
+      >
+        {children}
+      </motion.main>
     </div>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,12 @@
-import Head from "next/head";
 import "../styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps, router }) {
   return (
-    // <AnimatePresence mode="wait" initial={true}>
-    //   <Component {...pageProps} key={router.asPath} />
-    // </AnimatePresence>
     <>
-      <Head>{/* <link rel="preload" href="/fonts/Inter-roman.latin.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" /> */}</Head>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait" initial={true}>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
     </>
   );
 }
